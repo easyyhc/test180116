2ea1b33ef870612a20a60b7ab2415373e88253ed
yhc <easy_yhc@126.com>
2018-01-16 16:45:09 +0800

    进行了输出修改

--- Test170103/src/com/easy/test1/aaaa.java
+++ Test170103/src/com/easy/test1/aaaa.java
@@ -22,7 +22,7 @@ public class aaaa {
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-System.out.println("nihao");
+System.out.println("nihao12312312312312312312312312");
     }
 
 }
