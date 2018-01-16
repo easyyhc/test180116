56f9774af6c443ff9e488aa80c8203795c7005a4
yhc <easy_yhc@126.com>
2018-01-16 16:38:45 +0800

    这是第一次提交

---
+++ Test170103/src/com/easy/test1/aaaa.java
@@ -0,0 +1,28 @@
+/**
+ *@Copyright:Copyright (c) 2017 - 2100
+ *@Company:easy
+ */
+package com.easy.test1;
+
+
+/**
+ *@Title:
+ *@Description:
+ *@Author:yhc
+ *@Since:2018年1月16日
+ *@Version:1.1.0
+ */
+public class aaaa {
+
+    /**
+     * @param args
+     * @Author:yhc
+     * @Since:2018年1月16日
+     * @Description:
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+System.out.println("nihao");
+    }
+
+}
